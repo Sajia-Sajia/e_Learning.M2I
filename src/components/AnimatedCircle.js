39ac2dc5d893ad8759d6813726d61d55bd99657c
fatimazahra <fatima23zahra12@gmail.com>
2024-12-21 14:25:07 +0100

    resoudre

--- src/components/AnimatedCircle.js
+++ src/components/AnimatedCircle.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import styles from './AnimatedCircle.module.css'; // Importez les styles en tant que module
 
@@ -14,20 +13,3 @@ const AnimatedCircle = ({ size, top, left, className }) => {
 };
 
 export default AnimatedCircle;
-=======
-import React from 'react';
-import styles from './AnimatedCircle.module.css'; // Importez les styles en tant que module
-
-const AnimatedCircle = ({ size, top, left, className }) => {
-    const style = {
-        width: size || '50px', // Taille par défaut
-        height: size || '50px', // Taille par défaut
-        top: top || '20px',     // Position par défaut
-        left: left || '20px',   // Position par défaut
-    };
-
-    return <div className={`${styles.animatedCircle} ${styles[className]}`} style={style}></div>;
-};
-
-export default AnimatedCircle;
->>>>>>> c76881e (premier commit)
