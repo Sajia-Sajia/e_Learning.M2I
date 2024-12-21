e9cb595d0f210630ebad85547631d4fb7f6474ca
fatimazahra <fatima23zahra12@gmail.com>
2024-12-21 13:41:14 +0100

    Résolution des conflits dans src/App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from 'react';
 import './App.css';
@@ -17,7 +16,6 @@ import UserEdit from './annonces/UserEdit';
 import Sidebar from './annonces/Sidebar';
 import "./annonces/sb-admin-2.min.css";
 import Dashboard from './annonces/Dashboard';
-<<<<<<< HEAD
 import List from "./annonces/User/List";
 import Edit from "./annonces/User/Edit";
 import Create from "./annonces/User/Create";
@@ -27,12 +25,8 @@ import Liststudent from "./annonces/Etudiants/Liststudent";
 import Listmodule from "./annonces/modules/Listmodule";
 import Editmodule from "./annonces/modules/Editmodule";
 import Createmodule from "./annonces/modules/Createmodule";
+import ProfileAdmin from "./annonces/ProfileAdmin";
 
-import ProfileAdmin from "./annonces/ProfileAdmin"
-
-
-
-=======
 import TPList from './pageprof/TPList';
 import TPcreate from './pageprof/TPcreate';
 import TPedit from './pageprof/TPedit';
@@ -53,12 +47,10 @@ import Semestre3 from './etudiant/Semestre3';
 import Quiz from './etudiant/Quiz';
 import HomeProf from './pageprof/HomeProf';
 import ProfileProf from './pageprof/ProfileProf';
->>>>>>> pageProfSajiaNoura
 
 function App() {
     return (
-
-         <div className="App">
+        <div className="App">
             <Routes>
                 <Route path="/" element={<HomePage />} />
                 <Route path="/ProgramPage" element={<ProgramPage />} />
@@ -74,42 +66,30 @@ function App() {
                     <Route path='list' element={<List />} />
                     <Route path='createstudent' element={<Createstudent />} />
                     <Route path='editstudent' element={<Editstudent />} />
-                    <Route path='liststudent' element={<Liststudent/>} />
+                    <Route path='liststudent' element={<Liststudent />} />
                     <Route path='createmodule' element={<Createmodule />} />
                     <Route path='editmodule' element={<Editmodule />} />
-                    <Route path='listmodule' element={<Listmodule/>} />
-                    <Route path='ProfileAdmin' element={<ProfileAdmin/>} />
-
-
-
+                    <Route path='listmodule' element={<Listmodule />} />
+                    <Route path='ProfileAdmin' element={<ProfileAdmin />} />
                     <Route path='create' element={<Create />} />
-
                     <Route path='edit/:id' element={<Edit />} />
-
-
                     <Route path='create-user' element={<UserCreate />} />
                     <Route path='user-view/:id' element={<UserView />} />
                     <Route path='user-edit/:id' element={<UserEdit />} />
                     <Route path='editmodule/:id' element={<Editmodule />} />
-
                     <Route path='student-edit/:id' element={<Editstudent />} />
-
                 </Route>
                 <Route path="/portalprof" element={<Portalprof />}>
                     <Route path='dashboard' element={<Dashboard />} />
-
                     <Route path='annonces-list' element={<AnnoncesList />} />
                     <Route path='create-annonce' element={<AnnonceCreate />} />
                     <Route path='annonce-edit/:id' element={<Editannonce />} />
-
                     <Route path='tp-list' element={<TPList />} />
                     <Route path='create-tp' element={<TPcreate />} />
                     <Route path='tp-edit/:id' element={<TPedit />} />
-
                     <Route path='td-list' element={<TDList />} />
                     <Route path='create-td' element={<TDcreate />} />
-                    <Route path='td-edit/:id' element={< TDedit />} />
-
+                    <Route path='td-edit/:id' element={<TDedit />} />
                     <Route path='cours-list' element={<Courslist />} />
                     <Route path='create-cour' element={<CourCreate />} />
                     <Route path='cour-edit/:id' element={<Editcour />} />
@@ -121,88 +101,8 @@ function App() {
                 <Route path="/semestre-4" element={<Semestre4 />} />
                 <Route path="/Quiz" element={<Quiz />} />
             </Routes>
-            </div>
-
-  );
+        </div>
+    );
 }
 
 export default App;
-=======
-import "bootstrap/dist/css/bootstrap.min.css";
-import React from 'react';
-import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './Pages/Login';
-import Reset from './Pages/Reset';
-import ForgotPassword from './Pages/ForgotPassword';
-import HomePage from './Pages/HomePage'; 
-import ProgramPage from './Pages/ProgramPage';
-import Userlist from './annonces/Userlist';
-import Portal from './annonces/Portal';
-import UserCreate from './annonces/UserCreate';
-import UserView from './annonces/UserView';
-import UserEdit from './annonces/UserEdit';
-import Sidebar from './annonces/Sidebar';
-import Topbar from './annonces/Topbar';
-import "./annonces/sb-admin-2.min.css";
-import Dashboard from './annonces/Dashboard';
-import List from "./annonces/User/List";
-import Edit from "./annonces/User/Edit";
-import Create from "./annonces/User/Create";
-import Createstudent from "./annonces/Etudiants/Createstudent";
-import Editstudent from "./annonces/Etudiants/Editstudent";
-import Liststudent from "./annonces/Etudiants/Liststudent";
-import Listmodule from "./annonces/modules/Listmodule";
-import Editmodule from "./annonces/modules/Editmodule";
-import Createmodule from "./annonces/modules/Createmodule";
-
-import ProfileAdmin from "./annonces/ProfileAdmin"
-
-
-
-
-function App() {
-    return (
-
-         <div className="App">
-            <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/Login" element={<Login />} />
-                <Route path="/ForgotPassword" element={<ForgotPassword />} />
-                <Route path="/Reset" element={<Reset />} />
-                <Route path='/sidebar' element={<Sidebar />} />
-                <Route path="/portal" element={<Portal />}>
-                    <Route path='dashboard' element={<Dashboard />} />
-                    <Route path='user-list' element={<Userlist />} />
-                    <Route path='list' element={<List />} />
-                    <Route path='createstudent' element={<Createstudent />} />
-                    <Route path='editstudent' element={<Editstudent />} />
-                    <Route path='liststudent' element={<Liststudent/>} />
-                    <Route path='createmodule' element={<Createmodule />} />
-                    <Route path='editmodule' element={<Editmodule />} />
-                    <Route path='listmodule' element={<Listmodule/>} />
-                    <Route path='ProfileAdmin' element={<ProfileAdmin/>} />
-
-
-
-                    <Route path='create' element={<Create />} />
-
-                    <Route path='edit/:id' element={<Edit />} />
-
-
-                    <Route path='create-user' element={<UserCreate />} />
-                    <Route path='user-view/:id' element={<UserView />} />
-                    <Route path='user-edit/:id' element={<UserEdit />} />
-                    <Route path='editmodule/:id' element={<Editmodule />} />
-
-                    <Route path='student-edit/:id' element={<Editstudent />} />
-
-                </Route>
-            </Routes>
-            </div>
-
-  );
-}
-
-export default App;
->>>>>>> c76881e (premier commit)
